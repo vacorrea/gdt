d41513db8d425beba2ed8b457e70bd39e6631800
Vitor Correa <vcorrea@lnx.me.in>
2018-12-28 14:59:11 -0200

    estruturando roteiro de teste

--- src/main/java/in/me/gdt/GdtApplication.java
+++ src/main/java/in/me/gdt/GdtApplication.java
@@ -1,14 +1,11 @@
 package in.me.gdt;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GdtApplication {
-	
-	@Autowired private RestService restService;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(GdtApplication.class, args);
 	}
--- src/main/java/in/me/gdt/UserRepository.java
+++ src/main/java/in/me/gdt/UserRepository.java
@@ -1,8 +1,14 @@
 package in.me.gdt;
 
+import java.util.logging.Logger;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserRepository {
+    Logger logger = Logger.getLogger(UserRepository.class.toString());
 
+    public void repositoryActionTest() {
+        logger.info("action succeded");    
+    }
 }
