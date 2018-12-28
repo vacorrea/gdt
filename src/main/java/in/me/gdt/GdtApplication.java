3318153aa558352778a545083003cb54baa8d21e
Vitor Correa <vcorrea@lnx.me.in>
2018-12-28 10:56:25 -0200

    initial project structure scaffold

---
+++ src/main/java/in/me/gdt/GdtApplication.java
@@ -0,0 +1,14 @@
+package in.me.gdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdtApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/in/me/gdt/GdtApplicationTests.java
@@ -0,0 +1,17 @@
+package in.me.gdt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GdtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
