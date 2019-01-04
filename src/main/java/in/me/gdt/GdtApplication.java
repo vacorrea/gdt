af04dfa9c98f81b94ca155add3ef7b9645c133be
vacorrea <vtcorrea@gmail.com>
2019-01-04 17:05:27 -0200

    erro total de novo

--- src/main/java/in/me/gdt/GdtApplication.java
+++ src/main/java/in/me/gdt/GdtApplication.java
@@ -2,15 +2,9 @@ package in.me.gdt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GdtApplication extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(GdtApplication.class);
-	}
+public class GdtApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GdtApplication.class, args);
 	}
--- src/main/java/in/me/gdt/infra/SecurityConfig.java
+++ src/main/java/in/me/gdt/infra/SecurityConfig.java
@@ -1,23 +1,20 @@
 package in.me.gdt.infra;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+/*
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+*/
+public class SecurityConfig {
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
+    /*
     @Bean
     @Override
     public UserDetailsService userDetailsService() {
@@ -29,4 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .build();
         return new InMemoryUserDetailsManager(user);
     }
+}
+
+*/
 }
